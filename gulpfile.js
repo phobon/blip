e1128e3730a59a552ddda475ee9121891e4b8a52
Ben McCormick <Ben McCormick>
2017-04-01 09:40:14 +0800

    Pulling out colours into modules

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,16 @@ var nano = require('cssnano');
 var autoprefixer = require('autoprefixer');
 var rename = require('gulp-rename');
 
-// Clean all of our old js files
+// Clean all of our old css files
 gulp.task('clean', function() {    
-    return gulp.src(['./public/scripts/*.js'])
+    return gulp.src(['./bin/*.css', './bin/colours/*.css'])
         .pipe(clean());
 });
 
 // Compile less files (css preprocessor)
 gulp.task('minify', function() {
     var processors = [autoprefixer, nano];    
-    return gulp.src('./src/blip.less')
+    return gulp.src('./lib/blip.less')
         .pipe(less())
         .pipe(postcss(processors))
         .pipe(rename({ suffix: '.min' }))
@@ -26,10 +26,30 @@ gulp.task('minify', function() {
 
 gulp.task('verbose', function() {
     var processors = [autoprefixer];    
-    return gulp.src('./src/blip.less')
+    return gulp.src('./lib/blip.less')
         .pipe(less())
         .pipe(postcss(processors))
         .pipe(gulp.dest('./bin'))
 });
 
+gulp.task('minify-colours', function() {
+    var processors = [autoprefixer, nano];    
+    return gulp.src('./lib/colours/*.less')
+        .pipe(less())
+        .pipe(postcss(processors))
+        .pipe(rename({ prefix: 'blip.c.', suffix: '.min' }))
+        .pipe(gulp.dest('./bin/colours'))
+});
+
+gulp.task('verbose-colours', function() {    
+    var processors = [autoprefixer];    
+    return gulp.src('./lib/colours/*.less')
+        .pipe(less())
+        .pipe(postcss(processors))
+        .pipe(rename({ prefix: 'blip.colours.' }))
+        .pipe(gulp.dest('./bin/colours'))
+});
+
 gulp.task('transpile', ['clean', 'minify', 'verbose']);
+gulp.task('transpile-full', ['transpile', 'minify-colours', 'verbose-colours']);
+gulp.task('release', ['clean', 'minify', 'minify-colours']);
